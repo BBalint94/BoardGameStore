65de563517032ec1b4c8e00238be12c72be4bda7
BBalint94 <balint1994@gmail.com>
2020-01-08 18:41:33 +0100

    Service implementációja

--- service/src/main/java/dao/BoardGameDAO.java
+++ service/src/main/java/dao/BoardGameDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
-public class BoardGameDAO {
+import boardgamestore.model.BoardGame;
+
+import java.util.Collection;
+
+public interface BoardGameDAO {
+
+    void createBoardGame(BoardGame boardGame);
+    Collection<BoardGame> readAllBoardGame();
+    void updateBoardGame(BoardGame boardGame);
+    void deleteBoardGame(BoardGame boardGame);
+    BoardGame readBoardGameById (int id);
 }
--- service/src/main/java/service/impl/BoardGameServiceImplementation.java
+++ service/src/main/java/service/impl/BoardGameServiceImplementation.java
@@ -1,4 +1,41 @@
 package service.impl;
 
-public class BoardGameServiceImplementation {
+import boardgamestore.model.BoardGame;
+import boardgamestore.service.BoardGameService;
+import dao.BoardGameDAO;
+
+import java.util.Collection;
+
+public class BoardGameServiceImplementation implements BoardGameService {
+
+    BoardGameDAO dao;
+
+    public BoardGameServiceImplementation(BoardGameDAO dao) {
+        this.dao = dao;
+    }
+
+    public Collection<BoardGame> listAllBoardGame() {
+        return dao.readAllBoardGame();
+    }
+
+    public BoardGame getBoardGame(int id) {
+        return dao.readBoardGameById(id);
+    }
+
+    public void addBoardGame(BoardGame boardGame) {
+        dao.createBoardGame(boardGame);
+    }
+
+    public void updateBoardGame(BoardGame boardGame) {
+        dao.updateBoardGame(boardGame);
+    }
+
+    public void deleteBoardGame(BoardGame boardGame) {
+        dao.deleteBoardGame(boardGame);
+    }
+
+    public void deleteBoardGame(int id) {
+        BoardGame boardGame = dao.readBoardGameById(id);
+        dao.deleteBoardGame(boardGame);
+    }
 }
