8e196e41432743052c1c78f01739b431669ff63d
BBalint94 <balint1994@gmail.com>
2020-01-09 12:34:14 +0100

    BoardGameController showBoardGameCount metódusa megírva

--- controller/src/main/java/boardgamestore/controller/BoardGameController.java
+++ controller/src/main/java/boardgamestore/controller/BoardGameController.java
@@ -1,4 +1,32 @@
 package boardgamestore.controller;
 
+import boardgamestore.exception.NoMatchingID;
+import boardgamestore.service.BoardGameService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class BoardGameController {
+
+    BoardGameService service;
+
+    public BoardGameController(@Autowired BoardGameService service) {
+        this.service = service;
+    }
+
+    @RequestMapping(value = "/count",method = RequestMethod.GET)
+    @ResponseBody
+    public String showBoardGameCount(){
+        return String.valueOf(service.listAllBoardGame().size());
+    }
+
+    @ExceptionHandler(NoMatchingID.class)
+    @ResponseBody
+    public String handlerNoMatchingId(Exception e){
+        return "ID not found in the database: "+e.getMessage();
+    }
 }
--- controller/src/main/java/boardgamestore/controller/ExceptionController.java
+++ controller/src/main/java/boardgamestore/controller/ExceptionController.java
@@ -1,4 +1,7 @@
 package boardgamestore.controller;
 
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
 public class ExceptionController {
 }
